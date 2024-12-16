ee8d5ca7ad19b60e13636181545693f381ae65aa
Flávio Pereira <github@flaviopereira.dev>
2024-12-16 21:36:13 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -14,6 +14,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for box101
+DROP DATABASE IF EXISTS `box101`;
+CREATE DATABASE IF NOT EXISTS `box101` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `box101`;
+
 -- Dumping structure for table box101.service_item_types
 DROP TABLE IF EXISTS `service_item_types`;
 CREATE TABLE IF NOT EXISTS `service_item_types` (
@@ -55,7 +61,7 @@ INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `nif`, `address`,
 DROP TABLE IF EXISTS `vehicles`;
 CREATE TABLE IF NOT EXISTS `vehicles` (
   `matricula` varchar(9) NOT NULL DEFAULT '',
-  `odometer` int(10) unsigned DEFAULT NULL COMMENT 'Current mileage in kilometers',
+  `odometer` mediumint(9) NOT NULL,
   `year` year(4) DEFAULT NULL,
   `month` tinyint(2) unsigned DEFAULT NULL,
   `brand` varchar(50) NOT NULL DEFAULT '',
@@ -69,38 +75,41 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table box101.vehicles: ~18 rows (approximately)
-INSERT INTO `vehicles` (`matricula`, `year`, `month`, `brand`, `model`, `colour`, `trim`, `notes`, `registered_by`, `registration_date`) VALUES
-	('0432SC', '1995', NULL, 'Honda', 'Civic', 'Preto', 'EJ2', 'Do Valério', 0, '2024-08-27 13:00:01'),
-	('0496EV', '1995', 2, 'Honda', 'Civic', 'Preto', 'EJ2', NULL, 0, '2024-08-27 13:00:01'),
-	('1109FP', '1995', 8, 'Vw', 'Golf 3', 'Pleto', 'Variant', NULL, 0, '2024-08-27 13:00:01'),
-	('3308VL', '2003', 8, 'Renault', 'Kangoo', 'Branca', '1.5 DCI', 'Já levou três motores', 0, '2024-08-27 13:00:01'),
-	('4022AM', NULL, NULL, 'Honda', 'Civic', 'Vermelho', 'EG6', 'Mito com Tecto de Abrir', 0, '2024-08-27 13:00:01'),
-	('4120IL', '1997', NULL, 'Honda', 'Civic', 'Cinza', 'MA8', NULL, 0, '2024-08-27 13:00:01'),
-	('48AZ56', '2005', 12, 'Honda', 'Civic', 'Preto', 'EP1', 'Sérgio', 0, '2024-08-27 13:00:01'),
-	('5008SA', '2001', 7, 'Renault', 'Clio', 'Azul', 'Fase 2', '"Farois grandes"', 0, '2024-08-27 13:00:01'),
-	('6582OP', '1999', 12, 'Hyundai', 'H1', 'Branco', '2500TD', 'Futuro swap PD', 0, '2024-08-27 13:00:01'),
-	('6698XC', '2004', 2, 'Opel', 'Combo', 'Branco', '1.7 CDTI', NULL, 0, '2024-08-27 13:00:01'),
-	('6804MT', '1999', NULL, 'Honda', 'Civic', 'Cinza', 'EK3', 'Swap K20', 0, '2024-08-27 13:00:01'),
-	('7515MG', '1998', NULL, 'Seat', 'Ibiza', 'Vermelho', '6K 1.9PD', NULL, 0, '2024-08-27 13:00:01'),
-	('78MF30', '2011', 0, 'Audi', 'A5', 'Preto', '2.0 TDi', NULL, 0, '2024-08-27 13:00:01'),
-	('7975TC', '2002', 2, 'Seat', 'Ibiza', 'Cinza Rato', '6L PD100', 'Daniel', 0, '2024-08-27 13:00:01'),
-	('9165MF', '1998', 11, 'Honda', 'Cr-v', 'Azul', 'B20', NULL, 0, '2024-08-27 13:00:01'),
-	('9529FS', '1995', NULL, 'Honda', 'Civic', 'Preto', 'EJ2', 'D15 com 160 cavalos', 0, '2024-08-27 13:00:01'),
-	('9896OP', '1990', 7, 'Honda', 'Crx', 'Preto', '16i16', 'Swap B16A1', 0, '2024-08-27 13:00:01'),
-	('MQ6198', '1990', 7, 'Honda', 'Civic', 'Azul ', 'EC9', '(Casa) Swap K20', 0, '2024-08-27 13:00:01');
+INSERT INTO `vehicles` (`matricula`, `odometer`, `year`, `month`, `brand`, `model`, `colour`, `trim`, `notes`, `registered_by`, `registration_date`) VALUES
+	('0432SC', 0, '1995', NULL, 'Honda', 'Civic', 'Preto', 'EJ2', 'Do Valério', 0, '2024-08-27 13:00:01'),
+	('0496EV', 0, '1995', 2, 'Honda', 'Civic', 'Preto', 'EJ2', NULL, 0, '2024-08-27 13:00:01'),
+	('1109FP', 0, '1995', 8, 'Vw', 'Golf 3', 'Pleto', 'Variant', NULL, 0, '2024-08-27 13:00:01'),
+	('3308VL', 0, '2003', 8, 'Renault', 'Kangoo', 'Branca', '1.5 DCI', 'Já levou três motores', 0, '2024-08-27 13:00:01'),
+	('4022AM', 0, NULL, NULL, 'Honda', 'Civic', 'Vermelho', 'EG6', 'Mito com Tecto de Abrir', 0, '2024-08-27 13:00:01'),
+	('4120IL', 0, '1997', NULL, 'Honda', 'Civic', 'Cinza', 'MA8', NULL, 0, '2024-08-27 13:00:01'),
+	('48AZ56', 0, '2005', 12, 'Honda', 'Civic', 'Preto', 'EP1', 'Sérgio', 0, '2024-08-27 13:00:01'),
+	('5008SA', 0, '2001', 7, 'Renault', 'Clio', 'Azul', 'Fase 2', '"Farois grandes"', 0, '2024-08-27 13:00:01'),
+	('6582OP', 0, '1999', 12, 'Hyundai', 'H1', 'Branco', '2500TD', 'Futuro swap PD', 0, '2024-08-27 13:00:01'),
+	('6698XC', 0, '2004', 2, 'Opel', 'Combo', 'Branco', '1.7 CDTI', NULL, 0, '2024-08-27 13:00:01'),
+	('6804MT', 0, '1999', NULL, 'Honda', 'Civic', 'Cinza', 'EK3', 'Swap K20', 0, '2024-08-27 13:00:01'),
+	('7515MG', 0, '1998', NULL, 'Seat', 'Ibiza', 'Vermelho', '6K 1.9PD', NULL, 0, '2024-08-27 13:00:01'),
+	('78MF30', 0, '2011', 0, 'Audi', 'A5', 'Preto', '2.0 TDi', NULL, 0, '2024-08-27 13:00:01'),
+	('7975TC', 0, '2002', 2, 'Seat', 'Ibiza', 'Cinza Rato', '6L PD100', 'Daniel', 0, '2024-08-27 13:00:01'),
+	('9165MF', 0, '1998', 11, 'Honda', 'Cr-v', 'Azul', 'B20', NULL, 0, '2024-08-27 13:00:01'),
+	('9529FS', 0, '1995', NULL, 'Honda', 'Civic', 'Preto', 'EJ2', 'D15 com 160 cavalos', 0, '2024-08-27 13:00:01'),
+	('9896OP', 0, '1990', 7, 'Honda', 'Crx', 'Preto', '16i16', 'Swap B16A1', 0, '2024-08-27 13:00:01'),
+	('DDWD', 0, NULL, NULL, 'Sdd', 'Dd', NULL, NULL, NULL, 0, '2024-09-05 23:47:58'),
+	('MQ6198', 0, '1990', 7, 'Honda', 'Civic', 'Azul ', 'EC9', '(Casa) Swap K20', 0, '2024-08-27 13:00:01');
 
 -- Dumping structure for table box101.vehicle_services
 DROP TABLE IF EXISTS `vehicle_services`;
 CREATE TABLE IF NOT EXISTS `vehicle_services` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `matricula` varchar(9) NOT NULL DEFAULT '',
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `created_by` int(10) unsigned NOT NULL,
   `description` text DEFAULT NULL COMMENT 'General description of the service',
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `starting_date` datetime DEFAULT NULL,
+  `created_by` int(10) unsigned NOT NULL,
+  `state` enum('PENDING','PROPOSAL','IN_PROGRESS','AWAITING_APPROVAL','APPROVED','COMPLETED','CANCELLED') NOT NULL DEFAULT 'PENDING',
   `active` tinyint(1) unsigned NOT NULL DEFAULT 1 COMMENT 'Deleted?',
-  `state` enum('STALE','CANCELLED','PENDING INTERNAL APPROVAL','PENDING APPROVAL','ACCEPTED') NOT NULL DEFAULT 'STALE',
   PRIMARY KEY (`id`),
-  KEY `matricula` (`matricula`)
+  KEY `matricula` (`matricula`),
+  CONSTRAINT `fk_matricula` FOREIGN KEY (`matricula`) REFERENCES `vehicles` (`matricula`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table box101.vehicle_services: ~0 rows (approximately)
@@ -119,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `vehicle_service_items` (
   `end_notes` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `service_id` (`service_id`),
-  CONSTRAINT `fk_service_id` FOREIGN KEY (`service_id`) REFERENCES `vehicle_services` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `service_id` FOREIGN KEY (`service_id`) REFERENCES `vehicle_services` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table box101.vehicle_service_items: ~0 rows (approximately)
@@ -136,8 +145,8 @@ CREATE TABLE IF NOT EXISTS `vehicle_service_item_tracking` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `service_item_id` (`service_item_id`),
-  CONSTRAINT `fk_service_item_id` FOREIGN KEY (`service_item_id`) REFERENCES `vehicle_service_items` (`id`) ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION
+  CONSTRAINT `service_item_id` FOREIGN KEY (`service_item_id`) REFERENCES `vehicle_service_items` (`id`) ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table box101.vehicle_service_item_tracking: ~0 rows (approximately)
@@ -151,7 +160,7 @@ BEFORE INSERT ON `users`
 FOR EACH ROW
 BEGIN
     IF NEW.pin IS NULL THEN
-        SET NEW.pin = FLOOR(RAND() * 10000); -- Generates a random number between 0 and 9999
+        SET NEW.pin = FLOOR(RAND() * 10000); 
     END IF;
 END//
 DELIMITER ;
